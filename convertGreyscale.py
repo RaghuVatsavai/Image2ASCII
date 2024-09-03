b5757344f7b64605e76a0527cd709e1ae49e3b67
Raghu Vatsavai <48869528+RaghuVatsavai@users.noreply.github.com>
2024-09-03 21:50:13 +0100

    Modularize convertRGB.py for reuse and readability

--- convertGreyscale.py
+++ convertGreyscale.py
@@ -1,36 +1,45 @@
 import os.path
 from PIL import Image
 
-imagePathValid = False
-while not imagePathValid:
-    imagePath = input("Enter the image path to convert to ASCII...\n")
-    if os.path.isfile(imagePath):
-        imagePathValid = True
-        print("Valid image path.")
-    else:
-        print("Error: Image not found")
+def getImagePath():
+    imagePathValid = False
+    while not imagePathValid:
+        imagePath = input("Enter the image path to convert to ASCII...\n")
+        if os.path.isfile(imagePath):
+            imagePathValid = True
+            print("Valid image path.")
+        else:
+            print("Error: Image not found")
+    return imagePath
 
-img = Image.open(imagePath).convert('L')
-img.save("greyscale.png")
+def initializeImage():
+    img = Image.open(getImagePath()).convert('L')
 
-newWidth = 500
-width, height = img.size
-aspectRatio = height / width
-# 0.55 multiplier since characters are taller than wider
-newHeight = int(newWidth * aspectRatio * 0.55)
-img = img.resize((newWidth, newHeight))
+    # Resizing the image
+    newWidth = 500
+    width, height = img.size
+    aspectRatio = height / width
+    # 0.55 multiplier since characters are taller than wider
+    newHeight = int(newWidth * aspectRatio * 0.55)
+    img = img.resize((newWidth, newHeight))
+    return img
 
-asciiChars = ' .:-+*#%@'
+def createASCII_Greyscale(img):
+    asciiChars = ' .:-+*#%@'
+    asciiString = ""
 
-asciiString = ""
+    for i in range(img.height):
+        for j in range(img.width):
+            brightness = img.getpixel((j, i))
+            # takes the min so that no matter what brightness 0-255 that it has an index
+            index = min(brightness // (256 // len(asciiChars)), len(asciiChars) - 1)
+            asciiString += asciiChars[index]
+        asciiString += "\n"  
 
-for i in range(img.height):
-    for j in range(img.width):
-        brightness = img.getpixel((j, i))
-        # takes the min so that no matter what brightness 0-255 that it has an index
-        index = min(brightness // (256 // len(asciiChars)), len(asciiChars) - 1)
-        asciiString += asciiChars[index]
-    asciiString += "\n"  
+    print(asciiString)
 
-print(asciiString)
+def main():
+    createASCII_Greyscale(initializeImage())
+
+main()
 
--- convertRGB.py
+++ convertRGB.py
@@ -7,36 +7,46 @@ def getRGBColour(r, g, b):
 def resetColour():
     return "\033[0m"
 
-imagePathValid = False
-while not imagePathValid:
-    imagePath = input("Enter the image path to convert to ASCII...\n")
-    if os.path.isfile(imagePath):
-        imagePathValid = True
-        print("Valid image path.")
-    else:
-        print("Error: Image not found")
+def getImagePath():
+    imagePathValid = False
+    while not imagePathValid:
+        imagePath = input("Enter the image path to convert to ASCII...\n")
+        if os.path.isfile(imagePath):
+            imagePathValid = True
+            print("Valid image path.")
+        else:
+            print("Error: Image not found")
+    return imagePath
 
-img = Image.open(imagePath).convert(('RGB'))
+def initializeImage():
+    img = Image.open(getImagePath()).convert(('RGB'))
 
-newWidth = 500 
-width, height = img.size
-aspectRatio = height / width
-# 0.55 multiplier since characters are taller than wider
-newHeight = int(newWidth * aspectRatio * 0.55)
-img = img.resize((newWidth, newHeight))
+    # Resizing image
+    newWidth = 200
+    width, height = img.size
+    aspectRatio = height / width
+    # 0.55 multiplier since characters are taller than wider
+    newHeight = int(newWidth * aspectRatio * 0.55)
+    img = img.resize((newWidth, newHeight))
+    return img
 
-asciiChars = ' .:-+*#%@'
+def createASCII_RGB(img):
+    asciiChars = ' .:-+*#%@'
+    asciiString = ""
 
-asciiString = ""
+    for i in range(img.height):
+        for j in range(img.width):
+            r, g, b = img.getpixel((j, i))
+            brightness = int(0.3 * r + 0.59 * g + 0.11 * b)
+            # takes the min so that no matter what brightness 0-255 that it has an index
+            index = min(brightness // (256 // len(asciiChars)), len(asciiChars) - 1)
+            colour = getRGBColour(r, g, b)
+            asciiString += colour + asciiChars[index] + resetColour()
+        asciiString += "\n"  
 
-for i in range(img.height):
-    for j in range(img.width):
-        r, g, b = img.getpixel((j, i))
-        brightness = int(0.3 * r + 0.59 * g + 0.11 * b)
-        # takes the min so that no matter what brightness 0-255 that it has an index
-        index = min(brightness // (256 // len(asciiChars)), len(asciiChars) - 1)
-        colour = getRGBColour(r, g, b)
-        asciiString += colour + asciiChars[index] + resetColour()
-    asciiString += "\n"  
+    print(asciiString)
 
-print(asciiString)
+def main():
+    createASCII_RGB(initializeImage())
+
+main()
