7a78c96b10457034ba506a003d931f632e87e4e1
Raghu Vatsavai <48869528+RaghuVatsavai@users.noreply.github.com>
2024-09-15 16:03:00 +0100

    Creates coloured image of ascii art using the canvas

--- convertRGB.py
+++ convertRGB.py
@@ -1,5 +1,5 @@
 import os.path
-from PIL import Image
+from PIL import Image, ImageFont, ImageDraw
 
 def getRGBColour(r, g, b):
     return f"\033[38;2;{r};{g};{b}m"
@@ -72,11 +72,49 @@ def create_ASCII_RGB(img):
 
     print(asciiString)
 
+def render_ASCII_Image(img):
+    asciiChars = ' .:-+*#%@'
+
+    # Determine a font for the ASCII characters
+    # I'll just use the default one for now
+    characterFont = ImageFont.load_default()
+
+    # width and height of characters in the default font
+    # border box tells us the dimensions of the character (left, top, bottom, right)
+    # we can work out the character width and height from this
+    bbox = characterFont.getbbox('A')
+    charWidth = bbox[2] - bbox[0]
+    charHeight = bbox[3] - bbox[1]
+
+    # characters are slightly taller than they are wide
+    aspectRatioCorrection = 0.55
+
+    # Determining size of canvas
+    canvasWidth = int(img.width * charWidth * aspectRatioCorrection)
+    canvasHeight = img.height * charHeight
+
+    # creating a blank canvas with black background
+    canvas = Image.new('RGB', (canvasWidth, canvasHeight), color="black")
+    draw = ImageDraw.Draw(canvas)
+
+    for y in range(img.height):
+        for x in range(img.width):
+            r, g, b = img.getpixel((x ,y))
+            brightness = int(0.3 * r + 0.59 * g + 0.11 * b)
+            index = min(brightness // (256 // len(asciiChars)), len(asciiChars) - 1)
+            characterToAdd = asciiChars[index]
+            draw.text((x * charWidth * aspectRatioCorrection, y * charHeight), characterToAdd, fill=(r, g, b), font=characterFont)
+
+    canvas.save("ascii_image.png")
+    canvas.show()
+
 def run():
     imagePath = getImagePath()
     image = getImage(imagePath)
     newWidth = getImageWidth(imagePath)
     resizedImage = resizeImage(image, newWidth)
     create_ASCII_RGB(resizedImage)
+    # Give resizedImage to render_ASCII_Image
+    render_ASCII_Image(resizedImage)
 
 run()
